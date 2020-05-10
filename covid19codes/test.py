f947cf615ea6a703f73fcf8b8543efb057b00027
Mohammad Minhazul Alam <minhaz.ulalam@gmail.com>
2020-05-10 15:28:06 +0600

    dataset

--- covid19codes/test.py
+++ covid19codes/test.py
@@ -15,6 +15,8 @@ from pyFTS.common import Membership
 os.chdir('F:\\Minhaz\\MS@NSU\\Fuzzy\\Project_Drive_GIT\\Fuzzy_Systems\\covid19codes')
 
 dataset = pd.read_csv('time_series_covid19_confirmed_hubei.csv', sep=',')
+dataset = pd.read_csv('time_series_covid19_confirmed_global.csv', sep=',')
+
 y = dataset.to_numpy()[0]
 
 rows = []
